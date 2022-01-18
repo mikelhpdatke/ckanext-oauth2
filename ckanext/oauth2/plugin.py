ec163e16e21855944557006d5b736250333f21c2
mikelhpdatke <luongductuandat.psa@gmail.com>
2022-01-18 16:47:05 +0700

    REMOVE: Check admin permissions

--- ckanext/oauth2/plugin.py
+++ ckanext/oauth2/plugin.py
@@ -38,28 +38,28 @@ def _no_permissions(context, msg):
     return {'success': False, 'msg': msg.format(user=user)}
 
 
-@toolkit.auth_sysadmins_check
-def user_create(context, data_dict):
-    msg = toolkit._('Users cannot be created.')
-    return _no_permissions(context, msg)
+# @toolkit.auth_sysadmins_check
+# def user_create(context, data_dict):
+#     msg = toolkit._('Users cannot be created.')
+#     return _no_permissions(context, msg)
 
 
-@toolkit.auth_sysadmins_check
-def user_update(context, data_dict):
-    msg = toolkit._('Users cannot be edited.')
-    return _no_permissions(context, msg)
+# @toolkit.auth_sysadmins_check
+# def user_update(context, data_dict):
+#     msg = toolkit._('Users cannot be edited.')
+#     return _no_permissions(context, msg)
 
 
-@toolkit.auth_sysadmins_check
-def user_reset(context, data_dict):
-    msg = toolkit._('Users cannot reset passwords.')
-    return _no_permissions(context, msg)
+# @toolkit.auth_sysadmins_check
+# def user_reset(context, data_dict):
+#     msg = toolkit._('Users cannot reset passwords.')
+#     return _no_permissions(context, msg)
 
 
-@toolkit.auth_sysadmins_check
-def request_reset(context, data_dict):
-    msg = toolkit._('Users cannot reset passwords.')
-    return _no_permissions(context, msg)
+# @toolkit.auth_sysadmins_check
+# def request_reset(context, data_dict):
+#     msg = toolkit._('Users cannot reset passwords.')
+#     return _no_permissions(context, msg)
 
 
 def _get_previous_page(default_page):
